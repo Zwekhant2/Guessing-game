379bfe8372d881aad27c77eb66ea962d1eadfa94
zwekhantlwin <zwekhantlwin5@gmail.com>
2023-09-18 22:39:35 +0300

    using if/else

--- src/Gg.java
+++ src/Gg.java
@@ -9,6 +9,25 @@ public class Gg {
 		String guess = "";
 		int guessCount = 0;
 		Scanner scanner = new Scanner(System.in);
+		while (!guess.equals(correctName)) {
+			System.out.println("Guess a name: ");
+			guess = scanner.nextLine();
+			if (guess.equalsIgnoreCase("quit")) {
+				System.out.println("You quit the game.");
+				break;
+			}
+			guessCount++;
+			if (guess.equals(correctName)) {
+				System.out.println("Congratulation! You guessed the name '" + correctName + "' correctly.");
+				System.out.println("You made " + guessCount + "guesses.");
+			}
+			else {
+				int hintLength = Math.min(guessCount, correctName.length());
+				String hint = correctName.substring(0, hintLength);
+				System.out.println("Wrong guess! Here's a hint: " + hint);
+			}
+		  }
+			scanner.close();
 	}	
 
 }
