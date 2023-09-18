2fcc167dce4ef01ca5ab9445ec08b0d23d012e41
zwekhantlwin <zwekhantlwin5@gmail.com>
2023-09-18 22:36:45 +0300

    Make some names, declare random and scanner

--- src/Gg.java
+++ src/Gg.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class Gg {
 
 	public static void main(String[] args) {
-		
+		String[] names = {"Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hannah"};
+		Random random = new Random();
+		String correctName = names[random.nextInt(names.length)];
+		String guess = "";
+		int guessCount = 0;
+		Scanner scanner = new Scanner(System.in);
 	}	
 
 }
